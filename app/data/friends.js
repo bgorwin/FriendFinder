2ddf9506209b72d2a358b725d2656c3bac56626c
bgorwin <brent.gorwin@gmail.com>
2017-08-04 20:36:20 -0500

    Commit before deploy to Heroku

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+
+var friends = [
+  {
+    name: "Brent",
+    photo: "https://goo.gl/images/NF5N8Y",
+    scores: [
+      1,
+      3,
+      4,
+      2,
+      5,
+      1,
+      4,
+      1,
+      3,
+      4
+    ]
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
